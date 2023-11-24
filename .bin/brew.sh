694b0b56e5ab80f12bb1399550728e0bc0581686
Takatoshi Sasa <sasa@labnoMacBook-Pro.local>
2023-11-24 20:13:20 +0900

    updated

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -5,3 +5,10 @@ brew install neovim
 brew install --cask warp
 brew install --cask mactex-no-gui
 
+brew install pyenv
+pyenv install 3.11.4
+pyenv global 3.11.4
+pip install numpy
+pip install matplotlib
+pip install pandas
+pip install scipy
